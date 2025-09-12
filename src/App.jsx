0fa51c9578507db1a970b184b87b1c08629003b8
aponchikaj <lazzexa@gmail.com>
2025-09-13 03:48:26 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Classes from "./Learning/classes";
+export default function App(){
+  return(
+    <>
+      {/* For Classes
+      <header className="w-full h-screen flex items-center justify-center">
+        <h1 className="text-[40px] font-light ">{Classes()}</h1>
+      </header> */}
+    </>
+  )
+}
