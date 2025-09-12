483e9011ea32af6db05e03df6010ac436855385c
aponchikaj <lazzexa@gmail.com>
2025-09-13 03:52:19 +0400

    Stating arrow functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Classes from "./Learning/classes";
+import arrFunctions from "./Learning/arrowFunctions";
 export default function App(){
   return(
     <>
       {/* For Classes
-      <header className="w-full h-screen flex items-center justify-center">
+      <main className="w-full h-screen flex items-center justify-center">
         <h1 className="text-[40px] font-light ">{Classes()}</h1>
-      </header> */}
+      </main> */}
+      <main>
+        {arrFunctions()}
+      </main>
     </>
   )
 }
---
+++ src/Learning/arrowFunctions.jsx
@@ -0,0 +1,4 @@
+
+export default function arrFunctions(){
+    // gonna start when i wake up. ngl its 4AM...
+}
