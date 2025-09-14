3844be73e4949b442424aaa2708940641313ea02
Lazare Mirziashvili <lazzexa@gmail.com>
2025-09-14 14:30:46 +0400

    efoijfijeifj

--- src/App.jsx
+++ src/App.jsx
@@ -117,6 +117,73 @@ export default function App(){
   //   </main>
   // </>)
 
-  
+  //Workers System
+
+  // const BOTNAMES = ['Shamata',"Lomita","Karlo","Iuni","Civa"]
+  // const BOTROLES = ['Developer','Sales','Digital Artist','Assistant','Cleaner']
+  // const BOTSTATUSES = ['Working','Resting','Online','Offline']
+  // let [BOTS,SETBOT] = useState([  
+  //   {
+  //     id:1,
+  //     Username:BOTNAMES[Math.floor(Math.random()*BOTNAMES.length)],
+  //     ImageID: Math.floor(Math.random()*(500+1)-1),
+  //     Role: BOTROLES[Math.floor(Math.random()*BOTROLES.length)],
+  //     Status:BOTSTATUSES[Math.floor(Math.random()*BOTSTATUSES.length)]
+  //   }
+  // ])
+
+  // function AddNewBot(){
+  //   const newBot = {
+  //     id:BOTS.length+1,
+  //     Username:BOTNAMES[Math.floor(Math.random()*BOTNAMES.length)],
+  //     ImageID:Math.floor(Math.random()*(500+1)-1),
+  //     Role: BOTROLES[Math.floor(Math.random()*BOTROLES.length)],
+  //     Status:BOTSTATUSES[Math.floor(Math.random()*BOTSTATUSES.length)]
+  //   }
+  //   SETBOT([...BOTS,newBot])
+  // }
+
+  // const WORKERS = BOTS.map(w=><div key={w.id} className="flex flex-col border rounded-[10px] p-[10px]">
+  //   <img src={`https://picsum.photos/${w.ImageID}`} alt="image id" className="rounded-[100%] border-2 w-[100px]" />
+  //   <h1 className="font-bold text-2xl">{w.Username} <span className="text-xs font-bold">{w.id}</span></h1>
+  //   <p>Role: {w.Role}</p>
+  //   <p>Status: {w.Status}</p>
+  // </div>)
+
+  // return(<>
+  //   <main className="w-full p-[10px] flex flex-col ">
+  //     <section className="w-full p-[10px] flex flex-col items-center justify-center text-center font-bold">
+  //       <h1 className="text-2xl font-bold">WORKERS:</h1>
+  //       <button onClick={AddNewBot}>Add New</button>
+  //     </section>
+  //     <section className="w-full flex gap-10 items-center justify-center text-center flex-wrap">
+  //       {WORKERS}
+  //     </section>
+  //   </main>
+  // </>)
+
+  let [Todos,SetTodos] = useState([])
+  let [newTodo,SetNew]=useState('')
+
+  function AddNew(){
+    SetTodos([...Todos,newTodo])
+    SetNew('')
+  }
+
+  let TODOS = Todos.map((t)=><li>{t}</li>)
+
+  return(<>
+    <main className="w-full h-screen flex items-center justify-center flex-col">
+      <section>
+        <input className="rounded-[10px] border p-[10px]" type="text" value={newTodo} onChange={e=>SetNew(e.target.value)} />
+        <button onClick={AddNew}>ADD</button>
+      </section>
+      <section>
+        <ul className="flex flex-col">
+          {TODOS}
+        </ul>
+      </section>
+    </main>
+  </>)
 
 }
